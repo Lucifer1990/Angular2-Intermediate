fbce38f0d7b434627382818baf18a3d3ca82e9f9
Ankur Biswas <Ankur Biswas>
2017-05-14 16:10:09 +0530

    *ngIf added

--- app/event/event-thumbnail.component.ts
+++ app/event/event-thumbnail.component.ts
@@ -3,14 +3,16 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector:'event-thumbnail',
     template:`<div class="well hoverwell thumbnail">
-    <h2>{{childEvent.name}}</h2>
-    <div>Date:{{childEvent.date}}</div>
-    <div>Time:{{childEvent.time}}</div>
-    <div>Price:\${{childEvent.price}}</div>
+    <h2>{{childEvent?.name}}</h2>
+    <div>Date:{{childEvent?.date}}</div>
+    <div>Time:{{childEvent?.time}}</div>
+    <div>Price:\${{childEvent?.price}}</div>
     <div>
-    <span>Location:{{childEvent.location.address}}</span>
+    <div *ngIf="childEvent.location">
+    <span>Location:{{childEvent?.location.address}}</span>
     <span>&nbsp;</span>
-    <span>{{childEvent.location.city}}, {{childEvent.location.country}}</span>
+    <span>{{childEvent?.location.city}}, {{childEvent?.location.country}}</span>
+    <div>
     </div>
 </div>  `,
   styles:[`
